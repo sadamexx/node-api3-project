a0ca352d9a2da8e68a059a53eec47c2b130eb85e
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-02-13 13:08:47 -0800

    refactored with middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,19 +3,14 @@ const userDB = require('./userDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => { //validateuser
-  const name = req.body.name;
-  if(name){
+router.post('/', validateUser, (req, res) => { 
     userDB.insert(req.body)
     .then(user => {
       res.status(201).json(user);
     })
     .catch(error => {
       res.status(500).json({ message: "There was a failure spell cast while saving to the database"});
-    });
-  } else {
-    res.status(400).json({ message: "Please provide a name for your quest seeker"});
-  }  
+    });  
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -52,19 +47,15 @@ router.get('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  if(!id){
-    res.status(400).json({ message: "The user with the specified ID does not exist"});
-  } else {
+router.delete('/:id', validateUserId, (req, res) => {
+  const id = req.params.id; 
     userDB.remove(id)
     .then(user => {
-      res.status(200).json({ message: "The user has succumb to the Eye of Sauron!"})
+      res.status(200).json({ message: "The user has succumb to the Eye of Sauron and have been vanquished!"})
     })
-
-  }
-  
-  
+    .catch(error => {
+      res.status(500).json({ message: "Error while removing user"});
+    })
 });
 
 router.put('/:id', (req, res) => {
@@ -89,22 +80,27 @@ router.put('/:id', (req, res) => {
 function validateUserId(req, res, next) {
   const userID = req.params.id
 
-  if(!userID){
-    res.status(400).json({ message: "Invalid user id"});
-  } else {
-    userDB.getById(userID)
+  userDB.getById(userID)
     .then(user => {
-      console.log("Yay user id matches")
-      next();
+      if(user) {
+        req.user = user;
+      } else {
+        res.status(400).json({ message: "Invalid user id"});
+      }
+      next();  
     })
     .catch(error => {
       res.status(500).json({ message: " Dark magic was involved. Our powers were too weak."});
-    })
+    });
   }
-}
+
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body) {
+    res.statuss(400).json({ message: "You must provide user data" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
